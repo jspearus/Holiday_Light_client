c3e02a52b824ca72ebeff2c03f4bf62ab1c59347
jspearus <jspearus@gmail.com>
2021-10-25 17:37:07 -0500

    moved get next holiday to server

--- client.py
+++ client.py
@@ -1,31 +1,5 @@
 import socket
 import datetime
-import holidays
-
-# todo add list of holidays
-Holidays = [
-    "New Year's Day", "Memorial Day", "Independence Day", "Halloween",
-    "Thanksgiving", "Christmas Day"
-]
-#todo add holiday to list of holidays
-hList = holidays.US(years=2021).items()
-# hList.addItem(datetime.date(2021, 10, 31), "Halloween")
-# print(hList)
-
-
-def getNxtHoliday():
-    global nxtHoliday
-    global nxtDate
-    for date, name in sorted(hList):
-        print(name, date)
-        if datetime.date.today() < date and name in Holidays:
-            nxtDate = date
-            nxtHoliday = name
-            break
-
-
-print(datetime.date.today())
-getNxtHoliday()
 
 HEADER = 64
 PORT = 5000
@@ -53,8 +27,6 @@ while True:
     smsg = input("enter msg: \n")
     if smsg == 'q':
         break
-    elif smsg == 'holiday':
-        smsg = nxtHoliday
     send(smsg)
 
 send("Goodby Jeff!")
